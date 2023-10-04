7218d1efaaf38e9ce02ea53780212fa1954f40b5
Aadi Samant <samantbipin@gmail.com>
2023-10-05 02:31:14 +0530

    Weather Today

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ try {
 	const response = await fetch(url, options);
 	const result = await response.text();
 	console.log(result);
+	temp = response.temp
+	feels_like = response.feels_like
+	humidity = response.humidity
+	min_temp = response.min_temp
+	max_temp = response.max_temp
+	wind_degrees = response.wind_degrees
+	wind_speed = response.wind_speed
 } catch (error) {
 	console.error(error);
 }
