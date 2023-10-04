8e7fb401c66d28a44db6b5054ec2a3a6c7a570c6
Aadi Samant <samantbipin@gmail.com>
2023-10-05 00:31:56 +0530

    wether app

---
+++ script.js
@@ -0,0 +1,18 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '094e7fc57bmsh8324d09960063d6p1fca8ejsnc0c315751b2c',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+async function fetchdata(){
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
+fetchdata();
